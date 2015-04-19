ba92016a533c721bca692e48e93c90dbdbf8bba3
stackedqueue <yang.julian@gmail.com>
2015-04-18 18:41:46 -0700

    creates home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :upvotes
   resources :comments
   resources :categories
@@ -8,7 +10,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
