81a2e45310ba680ee6227852eafc630186234eaf
Julian Yang <yang.julian@gmail.com>
2015-04-20 21:52:21 -0700

    adds new flag to detect whether search was performed; adds additional condition to check if search parameters were empty before performing search (in controller);

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -5,6 +5,8 @@ class QuotesController < ApplicationController
   # GET /quotes.json
   def index
     @quotes = Quote.all
+    @has_search = false
+
     if params[:search] && !params[:search].empty?
       @has_search = true
       @search_quotes = Quote.search(params[:search]).order("created_at DESC")
