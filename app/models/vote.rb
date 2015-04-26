5ed1a1cd9fa559c118a3da01364c1699f287c1cf
Julian Yang <yang.julian@gmail.com>
2015-04-25 21:44:53 -0700

    adds upvoting and downvoting functionality to homepage / quote_block section still needs to limit one vote / user

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,9 @@
 class Vote < ActiveRecord::Base
   belongs_to :user
   belongs_to :quote
+
+  def self.quote_count(requested_quote_id)
+    @votes = Vote.where(quote_id: requested_quote_id)
+    @count = @votes.inject(0) { |result, vote| result + vote.value }
+  end
 end
