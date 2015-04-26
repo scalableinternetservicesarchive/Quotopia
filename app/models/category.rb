07a91402a9c7bc600cc29057cd471e0c6c573555
Roger <rchen93@ucla.edu>
2015-04-26 01:01:16 -0700

    Added case-insensitive constraint to categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ActiveRecord::Base
   has_and_belongs_to_many :quotes
 
   validates :content, presence: true
-  validates :content, uniqueness: true
+  validates :content, uniqueness: { case_sensitive: false }
 end
