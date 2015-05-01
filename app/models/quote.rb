6b5eb19673411f6ec289de31810137bf2d729b42
Roger <rchen93@ucla.edu>
2015-05-01 14:28:01 -0700

    Added has_many through relation for comments

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -5,6 +5,7 @@ class Quote < ActiveRecord::Base
   has_many :votes
   has_many :users, :through => :votes
   has_many :comments
+  has_many :users, :through => :comments
   # has_and_belongs_to_many :categories
   has_many :categorizations
   has_many :categories, :through => :categorizations
