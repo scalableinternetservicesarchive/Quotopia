71fbe5ff9cb972b3bcaa58693266eac94c56735a
Roger <rchen93@ucla.edu>
2015-05-01 22:01:42 -0700

    Reverted back to original has_many relation for comments

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ActiveRecord::Base
   has_many :votes
   has_many :quotes, :through => :votes
   has_many :comments
-  has_many :quotes, :through => :comments
   # has_many :submissions
   # has_many :quotes, through: :submissions
   # has_many :submissions, class_name: 'Quote', foreign_key: 'quote_id' if keep tracking of user-submissions does not work
