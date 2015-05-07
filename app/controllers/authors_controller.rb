27a75e849c8c780cc79ec6cefe58164d5669bbc1
Christina Yang <christinay412@gmail.com>
2015-05-07 13:58:41 -0700

    added an authors page that includes pagination for the displayed quotes

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -10,6 +10,7 @@ class AuthorsController < ApplicationController
   # GET /authors/1
   # GET /authors/1.json
   def show
+    @author_quotes = @author.quotes.all.page(params[:page])
   end
 
   # GET /authors/new
