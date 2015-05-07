f96f84afb9c12cdb1594cc3abbeb11ac6c452270
synthling <synthialing@Synthias-MacBook-Pro.local>
2015-05-07 00:32:41 -0700

    Move javascript functions for show more/less to separate file (not working atm)

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,15 +1,4 @@
-function showMoreOrLess( elem ) {
-    var link=elem;
 
-    do {
-        elem = elem.previousSibling;
-    } while ( elem && elem.nodeType !== 1);
-
-    if(link.innerHTML == "Show More"){
-        elem.style.maxHeight="none";
-        link.innerHTML="See Less";
-    } else {
-        elem.style.maxHeight="58px";
-        link.innerHTML="Show More";
-    }
+function test() {
+    alert.("this is a test");
 }
