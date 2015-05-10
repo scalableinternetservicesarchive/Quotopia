dc0cfcd6a6a4d17763fd9f851eaa52204a3957f7
Julian Yang <yang.julian@gmail.com>
2015-05-10 16:14:52 -0700

    Add devise to favorite_quotes_controller test

--- test/controllers/favorite_quotes_controller_test.rb
+++ test/controllers/favorite_quotes_controller_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class FavoriteQuotesControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
   setup do
     @favorite_quote = favorite_quotes(:one)
   end
