eae9093750bbdf12ec9de3b25778ad5aa2f25c55
synthling <synthialing@gmail.com>
2015-05-10 22:44:00 -0700

    Adds fading for quote panel overflow

--- app/assets/javascripts/quote_controller.js
+++ app/assets/javascripts/quote_controller.js
@@ -14,9 +14,11 @@ function showMoreOrLess( elem ) {
     if(link.innerHTML == "Show More"){
         elem.style.maxHeight="none";
         link.innerHTML="Show Less";
+        elem.classList.remove("fade-overflow");
     } else {
         elem.style.maxHeight="58px";
         link.innerHTML="Show More";
+        elem.classList.add("fade-overflow");
     }
 
 }
