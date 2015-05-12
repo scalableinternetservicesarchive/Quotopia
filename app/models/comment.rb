cabb3f96412d2963a15bff66277693300f8dc503
Roger <rchen93@ucla.edu>
2015-05-11 18:16:05 -0700

    Added model tests for Comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,5 @@ class Comment < ActiveRecord::Base
   belongs_to :user
 
   validates :content, presence: true
+  validates :quote, presence: true
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
+  fixtures :comments, :quotes
+
   test "comment content must not be empty" do
     comment = Comment.new
     assert comment.invalid?
@@ -9,6 +11,27 @@ class CommentTest < ActiveSupport::TestCase
 
   test "should not save empty comment" do
     comment = Comment.new
+    assert_not comment.save
+  end
+
+  test "comment is valid with content and associated quote" do
+    comment = Comment.new(content: "hi", quote: quotes(:one))
+    assert comment.valid
+  end
+
+  test "should save comment with content and associated quote" do
+    comment = Comment.new(content: "hi", quote: quotes(:one))
+    assert comment.save
+  end
+
+  test "comment is not valid without an associated quote" do
+    comment = Comment.new(content: "hi")
+    assert comment.invalid?
+    assert comment.errors[:quote].any?
+  end
+
+  test "should not save comment without an associated quote" do
+    comment = Comment.new(content: "hi")
     assert_not comment.save
   end
 end
