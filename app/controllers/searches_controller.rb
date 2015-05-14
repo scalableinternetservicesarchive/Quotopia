0eb8bc08ab393881b87829fa8d07eafa12f6c3e2
Julian Yang <yang.julian@gmail.com>
2015-05-14 16:21:39 -0700

    change search parameter name to 'q'

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,12 +3,16 @@ class SearchesController < ApplicationController
     def search 
       @has_search = false
     
-      if params[:search] && !params[:search].empty?
+      if params[:q] && !params[:q].empty?
         @has_search = true
-        @search_quotes = Quote.search(params[:search]).page(params[:page])
+        @search_quotes = Quote.search(params[:q]).page(params[:page])
       else
         @search_quotes = nil
       end
+    end
+
+    def search_typeahead
+        q = params[:query]
     end
 end 
 
