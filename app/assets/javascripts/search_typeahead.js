4f5d67e69dd254405780b21d682581cbac2ce22f
Julian Yang <yang.julian@gmail.com>
2015-05-24 02:28:28 -0700

    separate search into 3 portions; dynamically update the width of authors / categories

--- app/assets/javascripts/search_typeahead.js
+++ app/assets/javascripts/search_typeahead.js
@@ -1,10 +1,11 @@
 var init_typeahead = function(){ 
 
-  var suggestions = new Bloodhound({
+  var authors = new Bloodhound({
         datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.value);},
         queryTokenizer: Bloodhound.tokenizers.whitespace,
         remote: {
-            url: '/typeahead?q=%QUERY' ,
+            url: '/typeahead?type=author&q=%QUERY',
+            wildcard: '%QUERY'
             //filter: function (results) {
 
             //    return $.map(results, function (result) {
@@ -14,7 +15,27 @@ var init_typeahead = function(){
         }
   });
 
-  suggestions.initialize(); 
+  var quotes = new Bloodhound({
+        datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.value);},
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+            url: '/typeahead?type=quote&q=%QUERY',
+            wildcard: '%QUERY'
+        }
+  });
+
+  var categories = new Bloodhound({
+        datumTokenizer: function(d) { return Bloodhound.tokenizers.whitespace(d.value);},
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+            url: '/typeahead?type=category&q=%QUERY',
+            wildcard: '%QUERY'
+        }          
+  });
+
+  authors.initialize(); 
+  quotes.initialize();
+  categories.initialize();
 
   $('#q').typeahead(
     {
@@ -23,11 +44,52 @@ var init_typeahead = function(){
         minLength: 1
     }, 
     {
-        name: 'suggestions',
+        name: 'authors',
         displayKey: 'value',
-        source: suggestions.ttAdapter()
-    }); 
+        source: authors.ttAdapter(),
+        templates: {
+            header: '<h3 class="type">Authors</h3>'
+        }
+    },
+    {
+        name: 'categories',
+        displayKey: 'value',
+        source: categories.ttAdapter(),
+        templates: {
+            header: '<h3 class="type">Categories</h3>'
+        }
+    },
+    {
+        name: 'quotes',
+        displayKey: 'value',
+        source: quotes.ttAdapter(),
+        templates: {
+            header: '<h3 class="type">Quotes</h3>'
+        }
+    }
+  ); 
+
+  $('#q').bind('typeahead:render', function(ev, suggestion) {
+    console.log('rendered!');
+    var $authors = $('.tt-dataset-authors');
+    var $categories = $('.tt-dataset-categories');
+
+    var authors_hasChildren = ($authors.children().length > 0); 
+    var categories_hasChildren = ($categories.children().length > 0);
+    console.log('authors: ' + $authors.children() + ' length: ' + $authors.children().length);
+    console.log('categories: ' + categories_hasChildren);
+    if(authors_hasChildren && categories_hasChildren) {
+        $authors.css('width', '50%');
+        $categories.css('width', '50%');
+    } else {
+        $authors.css('width', '100%');
+        $categories.css('width', '100%');
+    }
+  });
+
 };
 
 $(document).on('page:load', init_typeahead);
 $(document).ready(init_typeahead);
+
+  
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -11,7 +11,10 @@ class SearchesController < ApplicationController
       end
     end
 
+        
     def typeahead
+        @type = params[:type]
+
         @q = params[:q].downcase
         @query = {
             query: {
@@ -32,21 +35,37 @@ class SearchesController < ApplicationController
                 }
             }
         }
-        @author_result = Author.search(@q).results
-        @results = Elasticsearch::Model.search(@query, [Author, Category, Quote]).results.to_a.map(&:to_hash)
-        @mapped_results = @results.map do |result|
-            @value = ""
-            case result["_type"]
-            when "author"
-                @value = "Author - " + result["_source"]["name"]
-            when "quote"
-                @value = "Quote - " + result["highlight"]["content"].join("")
-            when "category"
-                @value = "Category - " + result["highlight"]["content"].join('')
+        
+        case @type
+        when "author"
+            @results = Elasticsearch::Model.search(@query, [Author]).results.map do |result|
+                {:value => result["_source"]["name"]}
             end
-
-            {:value => @value}
+        when "quote"
+            @results = Elasticsearch::Model.search(@query, [Quote]).results.map do |result|
+                {:value => result["highlight"]["content"].join('')}
+            end
+        when "category"
+            @results = Elasticsearch::Model.search(@query, [Category]).results.map do |result|
+                {:value => result["highlight"]["content"].join('')}
+            end
+        else
+            @results = {:value => ""}
         end
+        #@results = Elasticsearch::Model.search(@query, [Author, Category, Quote]).results.to_a.map(&:to_hash)
+        #@mapped_results = @results.map do |result|
+        #    @value = ""
+        #    case result["_type"]
+        #    when "author"
+        #        @value = "Author - " + result["_source"]["name"]
+        #    when "quote"
+        #        @value = "Quote - " + result["highlight"]["content"].join("")
+        #    when "category"
+        #        @value = "Category - " + result["highlight"]["content"].join('')
+        #    end
+
+        #    {:value => @value}
+        #end
         #@authors = Author.select("authors.name as value")
         #                 .where("authors.name LIKE ?", "%#{@q}%")
         
@@ -58,7 +77,7 @@ class SearchesController < ApplicationController
         #               .where("quotes.content LIKE ?", "%#{@q}%")
         
         #might want to ensure capitalization in @categories?
-        puts render json: @mapped_results 
+        puts render json: @results 
 
     end
 end 
