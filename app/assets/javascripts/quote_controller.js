a97f7a2aa74476651180927c0179a91b7e9a807a
synthling <synthialing@gmail.com>
2015-05-24 13:07:43 -0700

    Adds active links to top navigation bar

--- app/assets/javascripts/quote_controller.js
+++ app/assets/javascripts/quote_controller.js
@@ -23,14 +23,8 @@
 var checkOverflow;
 checkOverflow = function() {
 
-  var elems = document.getElementsByClassName("pls");
+  var elems = document.getElementsByClassName("over");
   for(var i =0; i < elems.length; i++){
-    /*
-    console.log("scrollH " + elems[i].scrollHeight);
-    console.log("clientH " + elems[i].clientHeight);
-    console.log("scrollW " + elems[i].scrollWidth);
-    console.log("clientW " + elems[i].clientWidth);
-    */
     if (elems[i].scrollHeight > elems[i].clientHeight || elems[i].scrollWidth > elems[i].clientWidth) {
         $("<a class='see-more' onclick='showMoreOrLess(this);'>Show More</a>").insertAfter(elems[i]);
         elems[i].classList.add("fade-overflow");
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+	def is_active(action)       
+    	params[:controller] == action ? "active" : nil        
+  	end
 end
