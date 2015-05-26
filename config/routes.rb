54f2838a181b0d3256427f76bae0e1c86b8baca0
Christina Yang <christinay412@gmail.com>
2015-05-25 22:32:53 -0700

    changed modal to have specific id

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   get '/twitter/callback', to: 'sessions#create'
   post '/tweets/create', to: 'tweets#create'
   get '/tweets/error', to: 'tweets#error'
-  get '/quotes/modal', to: 'quotes#modal'
 
   resources :authentications
   get 'home/index'
