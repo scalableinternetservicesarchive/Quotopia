4ea5255383a54db9150e5cb73e8e94bee114f11a
Roger <rchen93@ucla.edu>
2015-05-29 20:13:11 -0700

    Changed cache invalidation for quote show page to checking the size of comments/categories, so it results in cache hit on front page

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -8,7 +8,7 @@ class Quote < ActiveRecord::Base
   has_many :users, :through => :votes
   has_many :comments, dependent: :destroy
   has_many :categorizations, dependent: :destroy
-  has_many :categories, :through => :categorizations, after_remove: proc { |q| q.touch }
+  has_many :categories, :through => :categorizations # , after_remove: proc { |q| q.touch }
   has_many :favorite_quotes
   has_many :favorited_by, through: :favorite_quotes, source: :user  # users that favorite a quote
 
