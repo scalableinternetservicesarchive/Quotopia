acbc3fd32d6d9bf36dbd6c013d0be3f99b5dcc93
Roger <rchen93@ucla.edu>
2015-05-29 21:12:50 -0700

    Added caching to categories show page

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,10 @@
 module CategoriesHelper
+  def cache_key_for_category_quote(category, quote, page)
+    category_updated_at = category.updated_at.try(:utc).try(:to_s, :number)
+    quote_updated_at = quote.updated_at.try(:utc).try(:to_s, :number)
+    if page.nil?
+      page = 1
+    end
+    "category/page-#{page}/#{category.id}/#{category_updated_at}/#{quote.id}/#{quote_updated_at}"
+  end
 end
--- app/helpers/quotes_helper.rb
+++ app/helpers/quotes_helper.rb
@@ -12,12 +12,4 @@ module QuotesHelper
     "quote/tab-#{tab}/#{quote.id}/#{updated_at}"
   end
 
-  def cache_key_for_author_quote(author, quote, page)
-    author_updated_at = author.updated_at.try(:utc).try(:to_s, :number)
-    quote_updated_at = quote.updated_at.try(:utc).try(:to_s, :number)
-    if page.nil?
-      page = 1
-    end
-    "author/page-#{page}/#{author.id}/#{author_updated_at}/#{quote.id}/#{quote_updated_at}"
-  end
 end
