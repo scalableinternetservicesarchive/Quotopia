33804414941282a763b81743199a0673e91a783d
Julian Yang <yang.julian@gmail.com>
2015-05-29 21:59:35 -0700

    update queries in favorites and categories for favorite integration

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -32,10 +32,12 @@ class CategoriesController < ApplicationController
     if user_signed_in?
       @quotes = @category.quotes
                          .joins(:author)
-                         .joins("LEFT JOIN( select id as vote_id, quote_id, value as vote_value from votes 
-                               WHERE user_id = " + current_user.id.to_s + ") as user_votes on quotes.id = user_votes.quote_id") 
+                         .joins("LEFT JOIN( SELECT id as favorite_ID, quote_id from favorite_quotes 
+                                            WHERE user_id = " + current_user.id.to_s + ") as favorites on quotes.id = favorites.quote_id")
+                         .joins("LEFT JOIN( SELECT id as vote_id, quote_id, value as vote_value from votes 
+                                            WHERE user_id = " + current_user.id.to_s + ") as user_votes on quotes.id = user_votes.quote_id") 
                          .order(vote_count: :desc)
-                         .select("quotes.id, quotes.content, authors.name as author_name, authors.id as author_id, vote_id, vote_value, vote_count")
+                         .select("quotes.id, quotes.content, authors.name as author_name, authors.id as author_id, favorite_id, vote_id, vote_value, vote_count")
                          .all
                          .page(params[:page])
 
